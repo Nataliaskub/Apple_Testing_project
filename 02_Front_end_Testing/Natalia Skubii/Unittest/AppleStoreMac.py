99a359a93d73fc1f4f059113f7facf6f1a498f7e
Natalia Skubii <150287438+Nataliaskub@users.noreply.github.com>
2024-10-28 23:02:13 -0400

    Unittest Positive

--- 02_Front_end_Testing/Natalia Skubii/Unittest/AppleStoreMac.py
+++ 02_Front_end_Testing/Natalia Skubii/Unittest/AppleStoreMac.py
@@ -6,7 +6,6 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.firefox.options import Options
